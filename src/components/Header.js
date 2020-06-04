ebbc9a19e84cc6f18301db167d68b325863e308e
honaldu <hsh9636@gmail.com>
2020-06-04 21:58:30 +0900

    header 수정중

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,74 +5,67 @@ import logo from "../images/samsung.png";
 const Header = () => {
   return (
     <header className="header-wrapper">
-      <div className="inner logo">
-        <a href="/">
-          <img src={logo} alt="logo" />
-        </a>
-      </div>
-      <div className="inner product">
-        <a href="/" className="galaxy">
-          갤럭시 5G
-        </a>
-        <a href="/" className="electro">
-          가전
-        </a>
-        <a href="/" className="mobile">
-          모바일
-        </a>
-        <a href="/" className="tv">
-          TV
-        </a>
-        <a href="/" className="it">
-          IT
-        </a>
-        <a href="/" className="audio">
-          오디오
-        </a>
-        <a href="/" className="consume">
-          소모품
-        </a>
-        <a href="/" className="event">
-          이벤트
-        </a>
-        <a href="/" className="dot">
-          닷컴핫딜
-        </a>
-      </div>
-
-      <div className="inner service">
-        <a href="/" className="story">
-          스토리
-        </a>
-        <a href="/" className="member">
-          멤버십
-        </a>
-        <a href="/" className="after">
-          고객지원
-        </a>
-        <a href="/" className="business">
-          비즈니스
-        </a>
-      </div>
-      <div className="inner info">
-        <a href="/">
-          <img
-            src="https://img.icons8.com/material-outlined/24/000000/user--v1.png"
-            alt="profile"
-          />
-        </a>
-        <a href="/">
-          <img
-            src="https://img.icons8.com/material-outlined/24/000000/shopping-cart.png"
-            alt="cart"
-          />
-        </a>
-        <a href="/">
-          <img
-            src="https://img.icons8.com/material-outlined/24/000000/search.png"
-            alt="search"
-          />
-        </a>
+      <div className="header-inner">
+        <div className="header-logo">
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+        <div className="header-product">
+          <a href="/">
+            <span>갤럭시 5G</span>
+          </a>
+          <a href="/">
+            <span>모바일</span>
+          </a>
+          <a href="/">
+            <span>모바일</span>
+          </a>
+          <a href="/">
+            <span>가전</span>
+          </a>
+          <a href="/">
+            <span>이벤트</span>
+          </a>
+          <a href="/">
+            <span>소모품</span>
+          </a>
+          <a href="/">
+            <span>오디오</span>
+          </a>
+          <a href="/">
+            <span>it</span>
+          </a>
+          <a href="/">
+            <span>닷컴핫딜</span>
+          </a>
+        </div>
+        <div className="header-service">
+          <a href="/">스토리</a>
+          <a href="/">멤버십</a>
+          <a href="/">고객지원</a>
+          <a href="/">비즈니스</a>
+        </div>
+        <div className="header-icon">
+          <a href="/">
+            <img
+              src="https://img.icons8.com/material-outlined/48/000000/user--v1.png"
+              alt="profile"
+            />
+          </a>
+          <a href="/">
+            <img
+              src="https://img.icons8.com/material-outlined/48/000000/shopping-cart.png"
+              alt="cart"
+            />
+          </a>
+          <a href="/">
+            <img
+              src="https://img.icons8.com/material-outlined/48/000000/search.png"
+              alt="search"
+            />
+          </a>
+        </div>
       </div>
     </header>
   );
